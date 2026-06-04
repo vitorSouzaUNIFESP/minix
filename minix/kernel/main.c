918c330c5b57a4d8e82b3944def3889f566d3395
vitorSouzaUNIFESP <162328650+vitorSouzaUNIFESP@users.noreply.github.com>
2026-06-04 20:25:27 -0300

    Update main.c

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -343,6 +343,10 @@ static void announce(void)
       "Copyright 2016, Vrije Universiteit, Amsterdam, The Netherlands\n",
       OS_RELEASE);
   printf("MINIX is open source software, see http://www.minix3.org\n");
+  printf("========================================\n");
+  printf("| Minix 3.4.0rc6 - SO - UNIFESP 1s2025 |\n");
+  printf("|    Projeto Pratico (I) - Equipe 7    |\n");
+  printf("========================================\n");
 }
 
 /*===========================================================================*
@@ -358,6 +362,10 @@ void prepare_shutdown(const int how)
    * argument passes the shutdown status. 
    */
   printf("MINIX will now be shut down ...\n");
+  printf("=======================================\n");
+  printf("| ATE A PROXIMA - SO - UNIFESP 1s2025 |\n");
+  printf("|    Projeto Pratico (I) - Equipe 7   |\n");
+  printf("=======================================\n");
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
@@ -519,4 +527,3 @@ int is_fpu(void)
 {
         return get_cpulocal_var(fpu_presence);
 }
-
